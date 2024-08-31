03de2c8f06c4a6e2ca4e29791ea5ef748b157a5d
Yegi Candra Monanza <candramonanzayegi@gmail.com>
2024-09-01 00:45:33 +0700

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,105 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>Node.js</li>
+                <li>MongoDB</li>
+                <li>Docker</li>
+                <li>Docker Compose</li>
+                <li>Kubernetes</li>
+                <li>Git</li>
+                <li>GitHub</li>
+                <li>Nginx</li>
+                <li>Nginx Proxy Manager</li>
+            </ul>
+        )
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>University of Life</li>
+                <li>University of Life</li>
+                <li>University of Life</li>
+                <li>University of Life</li>
+                <li>University of Life</li>
+            </ul>
+        )
+    },
+    {
+        title: "Certifications",
+        id: "certifications",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>AWS</li>
+                <li>Google Cloud Platform</li>
+                <li>Kubernetes</li>
+                <li>Docker</li>
+            </ul>
+        )
+    }
+]
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ea
+            impedit assumenda, rerum veritatis sint voluptates eos enim tempora
+            earum mollitia nihil iure officia voluptatum, iste odit consequuntur
+            tenetur esse consequatur at soluta eius? Modi tempore soluta porro!
+            Facilis tempora consectetur quod dolore aut. Voluptas nemo
+            accusantium enim maiores hic.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">{TAB_DATA.find((data) => data.id === tab).content}</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,60 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from 'react-type-animation';
+
+const HeroSection = () => {
+    return (
+        <section>
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+            <div className="col-span-7 place-self-center text-center sm:text-left">
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-amber-600">Hello, I'm {""}</span>
+                    <br />
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed out once, initially
+                            'Yegi',
+                            1000, // wait 1s before replacing "Mice" with "Hamsters"
+                            'Web Developer',
+                            1000,
+                            'Android Developer',
+                            1000,
+                            'UI/UX Designer',
+                            1000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        
+                        repeat={Infinity}
+                    />
+                </h1>
+                <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+                    provident quos vel expedita eos natus dolorem similique assumenda
+                    velit aspernatur?
+                </p>
+                <div className="div">
+                    <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-amber-600 to-amber-800 hover:bg-slate-200 text-white">Hire Me</button>
+                    <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-amber-500 to-amber-800 hover:bg-slate-800 text-white mt-3">
+                        <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
+                    </button>
+                </div>
+            </div>
+            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                    <Image
+                    src="/images/hero-image.png"
+                    alt="hero image"
+                    className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                    width={500}
+                    height={300}
+                    />
+                </div>
+            </div>
+        </div>
+        </section>
+    );
+};
+
+export default HeroSection;
